f3f0f8627d7e146e780175e4c4767996e4ea10be
Snooperlock <teamworkucla@hotmail.com>
2012-10-11 17:25:29 -0400

    start to learn layout

--- gen/com/mobiband/R.java
+++ gen/com/mobiband/R.java
@@ -15,7 +15,12 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int hostLabel=0x7f070001;
+        public static final int hostText=0x7f070002;
+        public static final int linearLayout1=0x7f070000;
+        public static final int menu_settings=0x7f070005;
+        public static final int portLabel=0x7f070003;
+        public static final int portText=0x7f070004;
     }
     public static final class layout {
         public static final int activity_mobi_band=0x7f030000;
@@ -26,7 +31,11 @@ public final class R {
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int hostLabel=0x7f040004;
+        public static final int hostText=0x7f040005;
         public static final int menu_settings=0x7f040002;
+        public static final int portLabel=0x7f040006;
+        public static final int portText=0x7f040007;
         public static final int title_activity_mobi_band=0x7f040003;
     }
     public static final class style {
